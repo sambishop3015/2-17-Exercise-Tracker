1f80ba5704219a58a1a8b256242d095461077ee7
Sam Bishop <sambishop3015@hotmail.com>
2021-01-22 22:38:58 -0500

    Fixed server.js routes

--- server.js
+++ server.js
@@ -1,25 +1,29 @@
 const express = require("express");
 const mongoose = require("mongoose");
-require("dotenv").config();
+const logger = require("morgan");
 
+//Porting
 const PORT = process.env.PORT || 3000;
 
 const app = express();
-
+app.use(logger("dev"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/exercise", {
-  useNewUrlParser: true,
-  useFindAndModify: false,
-  useUnifiedTopology: true
+//DB Connection
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+    useCreateIndex: true
 });
 
-// routes
-const API = require("./public/api");
+//API & HTML Routes
+app.use(require("./routes/api-routes.js"));
+app.use(require("./routes/html-routes.js"));
 
+//Starting Server
 app.listen(PORT, () => {
-  console.log(`App running on port ${PORT}!`);
+    console.log(`App running on port ${PORT}!`);
 });
